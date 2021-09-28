c3bda8e5c7644dc2bd792d2c148a70f56340ec3b
datarttu <arttu.kosonen@gmail.com>
2021-09-28 18:42:43 +0300

    Jore parsing WIP

---
+++ parse-jore.R
@@ -0,0 +1,121 @@
+#' Parse JORE route files, WIP
+#' 
+#' A Jore export file forms a tree of elements.
+#' Column indexes significant for us are marked.
+#' 
+#' - (`route`: route ids and names)
+#' - `rvariant`: route _variants_ (in most cases, _directions_) thereof
+#'   - 1 label, 2 hastus variant, 7 line_id
+#' - `rvpoint`: stops used by the route variants thereof
+#'   - 1 label, 2 active_place, 7 stop_id, 8 variant_id
+#' - (`place`: unique Hastus places used by rvpoints)
+#' - `stop`: unique stops used by rvpoints
+#'   - 1 label, 2 stop_id, 4 stop_name, 8 stop_place, 9 stop_lon, 10 stop_lat, 11 stop_code
+#' - (`stpdist`: distances between pairs of stops used by rvpoints)
+
+library(stringr)
+library(dplyr)
+library(tidyr)
+library(sf)
+
+#' Parse single Jore route file
+#' 
+#' The file name must be of format `<route_id>_<valid_from_yyyymmdd>_<valid_to_yyyymmdd>.txt`.
+#'
+#' @param file Path of the `.txt` file
+#'
+#' @return List of stop, route_version and stop_on_route tibbles
+parse_jore_file <- function(file) {
+  fname_els <- str_split_fixed(string = file, pattern = '_', n = 3)
+  if (ncol(fname_els) < 3) {
+    stop('File name must be like <route_id>_<valid_from_yyyymmdd>_<valid_to_yyyymmdd>.txt')
+  }
+  tryCatch(
+    {
+      valid_from <- as.Date(fname_els[2], format = '%Y%m%d')
+      valid_to <- as.Date(fname_els[3], format = '%Y%m%d')
+      stopifnot(valid_from <= valid_to)
+    }, 
+    error = function(e) {
+      stop('From and to dates must be of format yyyymmdd, and from <= to')
+    })
+  
+  raw_lines <- readLines(con = file, encoding = 'latin1')
+  
+  stopifnot(any(str_detect(raw_lines, 'rvariant;')))
+  stopifnot(any(str_detect(raw_lines, 'rvpoint;')))
+  stopifnot(any(str_detect(raw_lines, 'stop;')))
+  
+  stop <- tibble(x = str_subset(raw_lines, 'stop;')) %>%
+    separate(col = 'x',
+             into = c(NA, 'stop_id', NA, 'stop_name', NA, NA, NA, 'stop_place',
+                      'stop_lat', 'stop_lon', 'stop_code'),
+             sep = ';') %>%
+    mutate(stop_id = as.integer(stop_id),
+           stop_place = if_else(nchar(stop_place) == 0, NA_character_, stop_place),
+           stop_lat = as.numeric(stop_lat),
+           stop_lon = as.numeric(stop_lon),
+           # Fixed default values:
+           stop_radius_m = 20.0,
+           stop_mode = 'bus',
+           source_date = Sys.Date(),
+           parent_stop_id = NA_integer_) %>%
+    st_as_sf(coords = c('stop_lon', 'stop_lat'), crs = 4326) %>%
+    mutate(geom_text = st_as_text(geometry)) %>%
+    st_drop_geometry() %>%
+    select(stop_id, stop_radius_m, stop_mode, stop_code, stop_name, stop_place,
+           parent_stop_id, source_date, geom_text)
+  
+  route_version <- tibble(x = str_subset(raw_lines, 'rvariant;')) %>%
+    separate(col = 'x',
+             into = c(NA, 'hastus_variant', NA, NA, NA, 'variant_id', 'line_id'),
+             sep = ';') %>%
+    mutate(dir = as.integer(str_sub(hastus_variant, start = -1L, end = -1L)),
+           variant = str_extract(hastus_variant, '[[:alpha:]]'),
+           route = str_c(line_id, str_replace_na(variant, '')),
+           route_mode = 'bus',
+           valid_during = sprintf('[%s,%s]', format(valid_from, '%Y-%m-%d'), format(valid_to, '%Y-%m-%d')),
+           route_ver_id = sprintf('%s_%d_%s_%s', route, dir, format(valid_from, '%Y%m%d'), format(valid_to, '%Y%m%d')))
+  
+  stop_on_route <- tibble(x = str_subset(raw_lines, 'rvpoint;')) %>%
+    separate(col = 'x',
+             into = c(NA, 'active_place', NA, NA, NA, NA, 'stop_id', 'variant_id'),
+             sep = ';') %>%
+    mutate(stop_id = as.integer(stop_id),
+           active_place = if_else(nchar(active_place) == 0, NA_character_, active_place)) %>%
+    group_by(variant_id) %>%
+    mutate(stop_seq = row_number()) %>%
+    ungroup() %>%
+    left_join(route_version %>% select(variant_id, route_ver_id),
+              by = 'variant_id') %>%
+    select(route_ver_id, stop_seq, stop_id, active_place)
+  
+  route_version <- route_version %>% 
+    select(route_ver_id, route, dir, valid_during, route_mode)
+  
+  return(list(stop = stop, route_version = route_version, stop_on_route = stop_on_route))
+}
+
+#' Parse a set of Jore route files
+#'
+#' @param file Paths of the `.txt` file(s)
+#'
+#' @return List of stop, route_version and stop_on_route tibbles
+parse_setof_jore_files <- function(files) {
+  stopifnot(all(file.exists(files)))
+  res <- lapply(files, parse_jore_file)
+  
+  all_stops <- do.call(rbind, lapply(res, function(x) x$stop))
+  # NOTE: Possible duplicate stop_ids with different attribute values are simply
+  #       ignored here, only distinct rows by stop_id are kept.
+  stops_uniq_by_id <- distinct(all_stops, stop_id, .keep_all = TRUE)
+  
+  route_version <- do.call(rbind, lapply(res, function(x) x$route_version))
+  
+  stop_on_route <- do.call(rbind, lapply(res, function(x) x$stop_on_route))
+  
+  return(list(stop = stops_uniq_by_id, 
+              route_version = route_version, 
+              stop_on_route = stop_on_route))
+}
+
